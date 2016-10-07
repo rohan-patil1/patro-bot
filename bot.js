1f99f31de33262da8c17a159e8f125af84064721
prohan2 <nahor02@hotmail.com>
2016-10-06 23:45:39 -0700

    Modified bot.js

--- bot.js
+++ bot.js
@@ -1,16 +1,16 @@
 const restify = require('restify');
-const botbuilder = require('botbuilder');
+const builder = require('botbuilder');
 const recast = require('recastai');
-const config = require('./config.js');
+//const config = require('./config.js');
 
 // Connection to Microsoft Bot Framework
 const connector = new builder.ChatConnector({
-  appId: config.botConnector.appId,
-  appPassword: config.botConnector.appSecret,
+  appId: process.env.MICROSOFT_APP_ID,
+  appPassword: process.env.MICROSOFT_APP_SECRET,
 })
 
 //Connection to Recast.ai
-const recastClient = new recast.Client(config.recast.request_token);
+const recastClient = new recast.Client(process.env.RECASTAI_TOKEN);
 
 const bot = new builder.UniversalBot(connector);
 
@@ -18,15 +18,17 @@ const bot = new builder.UniversalBot(connector);
 bot.dialog('/', (session) => {
   console.log(session.message.text);
   recastClient.textRequest(session.message.text)
-              .then(res => {
-               console.log(res)
-               const intent = res.intent()
-               session.send('Intent: ${intent}')
-              })
-              .catch(() => session.send('I need some sleep right now... Talk to me later!'))
+  .then(res => {
+    console.log(res)
+    const intent = res.intent()
+    session.send('Intent: ${intent}')
+  })
+  .catch(() => session.send('I need some sleep right now... Talk to me later!'))
 })
 
 // Server Init
-const server = restify.createServer();
-server.listen(8080);
+const server = restify.createServer({name: 'test-bot'});
+server.listen(process.env.PORT || 6010, '0.0.0.0', () => {
+  console.log('%s listening at %s', server.name, server.url);
+});
 server.post('/', connector.listen());
